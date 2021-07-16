89b2d04786bbc652dc049c202efe3fff0b9625c8
Moshiur Rahman Sazal <80117611+MoshiurRahmanSazal@users.noreply.github.com>
2021-07-16 21:53:34 +0600

    Add files via upload
    Simple code to view  Covid19 news Live

---
+++ WorldCovid19Live.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+import requests
+from lxml import html
+
+covid_data = namedtuple("covid_data", "cases deaths recovered")
+def covid_stats (url: str = "https://www.worldometers.info/coronavirus/") \
+    -> covid_data:
+    xpath_str = '//div[@class = "maincounter-number"]/span/text()'
+    return covid_data(*html.fromstring(requests.get(url).content).xpath(xpath_str))
+
+
+fmt = """ 
+In the World:
+\nTotal COVID-19 Positive: {}
+Total Deaths: {}
+Total Recovered: {}"""
+print(fmt.format(*covid_stats()))
